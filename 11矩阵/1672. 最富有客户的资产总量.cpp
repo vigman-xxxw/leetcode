6c65142cefe46964931ddd453c848e38295e1a44
vigman <1319284583@qq.com>
2022-06-11 11:03:51 +0800

    矩阵

---
+++ 11矩阵/1672. 最富有客户的资产总量.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int row = accounts.size();
+        int col = accounts[0].size();
+        int ret = -1000000;
+        for(int i = 0; i < row; ++i){
+            int sum = 0;
+            for(int j = 0; j < col; ++j){
+                sum += accounts[i][j];
+            }
+            ret = max(ret,sum);
+        }
+        return ret;
+    }
+};
