82d1046466e2539d152a583511a1094008782e6c
vigman <1319284583@qq.com>
2022-07-01 21:57:35 +0800

    数组

---
+++ 01数组/2319. 判断矩阵是否是一个 X 矩阵.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+     int n = grid.size();
+     int i, j;
+     for(i = 0; i < n; ++i){
+         for(j = 0; j < n; ++j){
+            if(i == j || i + j == n - 1){
+                if(!grid[i][j]){
+                    return false;
+                }
+            }
+            else{
+                if(grid[i][j]){
+                    return false;
+                }
+            }
+         }
+           
+    }
+    return true;
+    }
+};
